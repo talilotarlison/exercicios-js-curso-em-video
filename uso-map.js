0ba9ecca1a4ff72648f27062bfb0a7504cb15303
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-15 10:03:26 -0300

    uso map

---
+++ uso-map.js
@@ -0,0 +1,27 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+const listaPesoas = [
+    { id: "1", nome: "Talilo", idade: "20" },
+    { id: "2", nome: "Joao", idade: "20" },
+    { id: "3", nome: "marcos", idade: "20" }
+  ];
+  
+  //modelo um
+  
+  listaPesoas.map(function(item){
+    
+    var res = `seu nome é ${item.nome}`
+    console.log(res);
+  }
+    
+    
+    );
+  
+  //modelo 2
+  listaPesoas.map((item)=>{
+    
+    var res = `sua idade é ${item.idade}`
+    console.log(res);
+  }
+    
+    
+    );
