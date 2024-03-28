ddd675ecb0c3c7a95ecf2bdc6b5a716aa1d6dddd
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-28 11:04:45 -0300

    Create texto-terminal.js

---
+++ texto-terminal.js
@@ -0,0 +1,7 @@
+# Criar texto personalizado no terminal:
+
+# Forum de discurção:
+# https://pt.stackoverflow.com/questions/258622/como-criar-desenhos-em-arquivos-escritos-em-batch
+
+# Site de criação:
+# https://pt.stackoverflow.com/questions/258622/como-criar-desenhos-em-arquivos-escritos-em-batch
