069f59a53dc16c2df2829d12d4cceb5530199c78
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-03-28 10:17:47 -0300

    Create responsivevoice.js

---
+++ responsivevoice.js
@@ -0,0 +1,9 @@
+// biblioteca de ler texto em voz para aplicação web
+// link de acesso
+// https://responsivevoice.org/
+
+// link CDN
+<script src=’https://code.responsivevoice.org/responsivevoice.js?key=YOUR_UNIQUE_KEY’></script>
+
+// uso html
+<input onclick="responsiveVoice.speak(“Hello World”);" type="button" value="Play" />
